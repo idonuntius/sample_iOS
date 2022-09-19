9e978cbc7c9262f05e0a1fc8987fbea322396bc2
idonun <mc.natsu831@gmail.com>
2022-09-19 22:37:52 +0900

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Natsuki Idota on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
